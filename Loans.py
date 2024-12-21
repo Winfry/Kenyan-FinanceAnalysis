dd612accce0186bb60d68cebf2eba2519d35a158
Winfry <Winfrynyarangi@gmail.com>
2024-12-21 15:07:04 +0300

    Kenyan-FinanceAnalysis

--- Loans.py
+++ Loans.py
@@ -4,6 +4,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
+import plotly.express as px
+import plotly.graph_objects as go
 
 # Title of the web app
 st.title("Kenya Loans Prediction and Recommendation System")
